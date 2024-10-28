f70f92ec70f019e370fbb40687789f3173af0a86
63bytes <>
2024-10-28 09:23:42 +0000

    NOP and HLT fineshed

--- gcpu/G0X.py
+++ gcpu/G0X.py
@@ -32,27 +32,48 @@ class MEMORY:
         self.data = {}
         count = 0
         for x in range(address_size**2):
-            self.data.update({toHex(x, CONFIG_HEX_SIZE):"0"*data_size})
+            self.data.update({toHex(x, CONFIG_HEX_SIZE):"0"*CONFIG_HEX_SIZE})
     def get(self, address):
         return self.data[address]
     def set(self, address, data):
-        try:
+        if int(address, 16)<=int(self.address_end, 16):
             self.data[address] = data
-        except IndexError:
+        else:
             logging.log(logging.ERROR, f"MEMORY - Address {address} was set to but is out of range({self.address_end})")
-
-class INSTRUCTIONS:
-    def NOP(args):#No operation  
-        logging.log(logging.DEBUG, f"INSTRUCTION - NOP (No operation) Args:{args}")
-   
-def execute(instruc, args):
-    if instruc=="00":
-        INSTRUCTIONS.NOP()
 
 class cpu:
     def __init__(self,file):
+        logging.log(logging.INFO, "CPU initialized")
         self.program = getProgram(file)
+        self.running = False
         self.mem = MEMORY(16,8)
         for x in range(len(self.program)):
             self.mem.set(toHex(x,CONFIG_HEX_SIZE), self.program[x])
-        
+        self.pointer = 0
+    ########################## Instruction set
+    def NOP(self):#No operation  
+        logging.log(logging.DEBUG, f"INSTRUCTION {toHex(self.pointer-1, CONFIG_HEX_SIZE)} - NOP (No operation)")
+    def HLT(self):#Halt the program
+        self.running =  False
+        logging.log(logging.DEBUG, f"INSTRUCTION {toHex(self.pointer-1, CONFIG_HEX_SIZE)} - HLT (Halt)")
+        logging.log(logging.INFO, f"Program halted")
+    ##########################
+    def execute(self):
+        instruc = self.getNext()
+        print(instruc)
+        if instruc=="00":
+            self.NOP()
+        elif instruc=="ff":
+            self.HLT()
+        else:
+            logging.log(logging.WARN, f"OP Code unknown ({instruc})")
+    def getNext(self):
+        self.pointer+=1
+        return self.mem.get(toHex(self.pointer-1, CONFIG_HEX_SIZE))
+    def run(self):
+        logging.log(logging.INFO, "Program started")
+        self.running = True
+        for x in range(len(self.program)):
+            if self.running == False:
+                break
+            self.execute()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import gcpu.G0X as GOX
 
 cpu = GOX.cpu("program.hex")
-print(cpu.mem.data)
+cpu.run()
